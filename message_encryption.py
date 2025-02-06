9c7838a613ca1b77a064f45cc7e518e317d95edf
Nikos-Nikoloutsopoulos <nnikoloutsopoulos70@gmail.com>
2025-02-06 08:17:36 +0000

    Comments Update

--- message_encryption.py
+++ message_encryption.py
@@ -1,5 +1,15 @@
-# This program encrypts and decrypts an input message using a key value
+# This program encrypts and decrypts an input message using the Caesar Cipher method
 
+"""
+The Caesar Cipher is one of the simplest and oldest methods of encrypting messages,
+named after Julius Caesar, who reportedly used it to protect his military communications. 
+This technique involves shifting the letters of the alphabet by a fixed number of places.
+For example, with a shift of three, the letter ‘A’ becomes ‘D’, ‘B’ becomes ‘E’, and so on. 
+Despite its simplicity, the Caesar Cipher formed the groundwork for modern cryptographic techniques. 
+In this article, we’ll explore how the Caesar Cipher works, its significance, 
+and its impact on the development of cryptography with its advantages and disadvantages.
+
+"""
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 message=input("Enter a message: ")
