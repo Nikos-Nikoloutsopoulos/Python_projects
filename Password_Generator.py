28724f9239c67d7b66a14f87f1d275730563c7e7
Nikos-Nikoloutsopoulos <nnikoloutsopoulos70@gmail.com>
2025-02-12 15:07:08 +0000

    Code Update

--- Password_Generator.py
+++ Password_Generator.py
@@ -3,12 +3,13 @@ import string
 
 min_length =12
 max_length =20
-
+# Randomly check the password length
 password_lenght= random.randint(min_length, max_length)
 
 password=""
-
+#According to length add characters to password
 for char in range(password_lenght):
     password+=random.choice(string.ascii_letters)
 
+#print the generated password
 print(f"The created password is: {password}")
